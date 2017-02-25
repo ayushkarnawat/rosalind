80acf79992235518fc44f30cea29aaee9871ee47
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-02-25 15:57:19 -0500

    Finished Problem 2 and 3

---
+++ 002_transcribing_dna_into_rna.py
@@ -0,0 +1,56 @@
+# Rosalind, Problem 2: Transcribing DNA into RNA
+# 
+# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+# Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is 
+# 	formed by replacing all occurrences of 'T' in t with 'U' in u. 
+# 
+# Given: A DNA string t having length at most 1000 nt.
+# Return: The transcribed RNA string of t
+# 
+# Sample Input: GATGGAACTTGACTACGTAAATT
+# Sample Output: GAUGGAACUUGACUACGUAAAUU
+
+from enum import Enum
+
+class DNA(Enum):
+	"""Represents the various nucleotides that can be within a given DNA string."""
+	A = "A"
+	C = "C"
+	G = "G"
+	T = "T"
+
+class RNA(Enum):
+	"""Represents the various nucleotides that can be within a given RNA string."""
+	A = "A"
+	C = "C"
+	G = "G"
+	U = "U"
+
+def convert_dna_to_rna(dna):
+	"""
+	Transcribes the given DNA sequence into RNA.
+	
+	params:
+		dna (str): A collection of nucleotides (A,C,G,T)
+			i.e. "GATGGAACTTGACTACGTAAATT"
+
+	returns:
+		rna (str): A collection of nucleotides (A,C,G,U)
+			i.e. "GAUGGAACUUGACUACGUAAAUU"
+	"""
+	rna = []
+	for nucleotide in dna:
+		if (nucleotide == DNA.A.value):
+			rna.append(RNA.A.value)
+		elif (nucleotide == DNA.C.value):
+			rna.append(RNA.C.value)
+		elif (nucleotide == DNA.G.value):
+			rna.append(RNA.G.value)
+		else:
+			rna.append(RNA.U.value)
+	return ''.join(rna) # combines the list of nucleotides in rna into a string
+
+if __name__ == "__main__":
+	with open("data/002_rosalind_rna.txt", "r") as dna:
+		sequence = dna.read()
+	print(convert_dna_to_rna(sequence))
---
+++ 003_complementing_dna.py
@@ -0,0 +1,63 @@
+# Rosalind, Problem 3: Complementing a Strand of DNA
+# 
+# In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+# The reverse complement of a DNA string s is the string sc formed by reversing the symbols 
+#   of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+# 
+# Given: A DNA string s of length at most 1000 bp.
+# Return: The reverse complement sc of s.
+# 
+# Sample Input: AAAACCCGGT
+# Sample Output: ACCGGGTTTT
+
+from enum import Enum
+
+class DNA(Enum):
+	"""Represents the various nucleotides that can be within a given DNA string."""
+	A = "A"
+	C = "C"
+	G = "G"
+	T = "T"
+
+def reverse(dna):
+	"""
+	Reverses the given string.
+	
+	params:
+		dna (str): A string of nucleotides constructed from the alphabet {A,C,G,T}
+			i.e. "AAAACCCGGT"
+	
+	returns:
+		reversed_dna (str): The reversed form of the DNA string
+			i.e. "TGGCCCAAAA"
+	"""
+	return dna[::-1]
+
+def reverse_complement(dna):
+	"""
+	Finds the complement of the given DNA strand. 
+
+	params:
+		dna (str): A string of nucleotides constructed from the alphabet {A,C,G,T}
+			i.e. "AAAACCCGGT"
+
+	returns:
+		complement (str): The complement form of the DNA string
+			i.e. "ACCGGGTTTT"
+	"""
+	complement = []
+	for nucleotide in reverse(dna):
+		if (nucleotide == DNA.A.value):
+			complement.append(DNA.T.value)
+		elif (nucleotide == DNA.C.value):
+			complement.append(DNA.G.value)
+		elif (nucleotide == DNA.G.value):
+			complement.append(DNA.C.value)
+		elif (nucleotide == DNA.T.value):
+			complement.append(DNA.A.value)
+	return ''.join(complement) # combines the reversed list of nucleotides into a string
+
+if __name__ == "__main__":
+	with open("data/003_rosalind_revc.txt", "r") as dna:
+		sequence = dna.read()
+	print(reverse_complement(sequence))
