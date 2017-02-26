42b64c63cb6a39f6f96c1ec2b749220ca9f88960
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-02-26 00:55:16 -0500

    Started python village questions

---
+++ 001_python_village/001_installing_python.py
@@ -0,0 +1,7 @@
+# Python Village Problem 1: Installing Python
+# 
+# After downloading and installing Python, type import this into the Python 
+#   command line and see what happens. Then, click the "Download dataset" 
+#   button below and copy the Zen of Python into the space provided.
+
+import this
